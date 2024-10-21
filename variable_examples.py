d9662c7e3313c4b524ca65baca0bda05dcbfd7bb
Jennifer Santos Custodio <jsantoscustodio@appdev.yearup.org>
2024-10-21 15:37:32 -0500

    created 1.B Exercise

---
+++ variable_examples.py
@@ -0,0 +1,12 @@
+customer_id = 963 ## for this type of value i picked the variable to be an integer as the unique identifer is usually a number ##
+customer_fullname = 'john ken doe' ## for this type of value i picked a string of word characters ##
+customer_gender = 'female' ## for this type of value i also picked a string gof word character ## 
+customer_dob = '2024-10-21' ## for this type of value i picked a numeric value as it is a date but i will not add it with anything ##
+customer_dln = '24681012w' ## for this type of value i picked a string as it could have both a numeric and word characters ## 
+customer_auto_policy = 'jkd13579'## for this type of value i picked a string as it could have a numeric and word chatacters ## 
+
+studentfullname ='jennifer santos custodio'
+student_city_of_birth = 'moroleon'
+student_state_of_birth = 'guanajuato'
+
+
