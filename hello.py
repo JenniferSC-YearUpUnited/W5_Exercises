bbc7cd95a5b9eda324aba6bcbdd6b6f07287be43
Jennifer Santos Custodio <jsantoscustodio@appdev.yearup.org>
2024-10-21 14:34:11 -0500

    created hello.py

---
+++ hello.py
@@ -0,0 +1,15 @@
+print('Hello world!')
+
+
+message = 'Hello world!'
+print(message)
+
+dollars = '3'
+cents = '.50'
+print(dollars + cents)
+
+print(type(dollars+cents))
+
+word1 = '3 dollars'
+word2 = 'fifty cents'
+print (word1 +" "+ word2)
