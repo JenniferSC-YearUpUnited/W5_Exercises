28abccaefdaf229e381bf975b4e7d107714bab89
Jennifer Santos Custodio <jsantoscustodio@appdev.yearup.org>
2024-10-22 11:47:23 -0500

    Adding q7

--- tips.py
+++ tips.py
@@ -3,5 +3,16 @@ tax=6.54
 tip=12.00
 
 total_due=food_cost+tax+tip
+#The str()function is to cast a number into a string. It is being used here to cast the total due numerical value into a string value as we can not concate both a numerical value and a string value together. We would have to convert one data type into another.
+#print("The total due is " + str(total_due))
 
-print("The total due is" + str(total_due))
+print("Food cost is " + str(food_cost) + " and tax is " + str(tax))
+#print("Tip is " + str(tip))
+print("Tip is " + format(tip, ".2f"))
+print("Total due is " + str(total_due))
+
+#you can't simply copy and past the above text into VSCode because it does not evaluate the strings with the variables.
+#print(“Food cost is ” + str(food_cost) + “ and tax is ” + str(tax)) 
+#print(“Tip is ” + str(tip)) 
+#print(“Total due is ” + str(total_due)) 
+
