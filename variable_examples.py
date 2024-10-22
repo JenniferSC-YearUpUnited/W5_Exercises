828f487c6dc71e3c433d1a9652f461e4afb068aa
Jennifer Santos Custodio <jsantoscustodio@appdev.yearup.org>
2024-10-22 08:43:40 -0500

    Adding lab 2

--- variable_examples.py
+++ variable_examples.py
@@ -1,3 +1,4 @@
+## lab 1 ##
 cust_id = 963 ## for this type of value i picked the variable to be an integer as the unique identifer is usually a number ##
 cust_fn = 'john ken doe' ## for this type of value i picked a string of word characters ##
 cust_gender = 'female' ## for this type of value i also picked a string gof word character ## 
@@ -10,3 +11,11 @@ stud_cob= 'moroleon'
 stud_sob = 'guanajuato'
 
 
+## lab 2 ##
+# False def if raise None del import return True elif in try and else is while as except lambda with assert finally nonlocal yield break for not class form or continue global pass ## 
+
+# False it is a boolean operator that represenets the opposite of True #
+# ^^ elif is shorthand for else if, checks if some other condition holds when the condition in the if statment is false ^^ #
+# ^^ try is used to make a try except statment. The try statment starts a block of code that is tried to execute. If it fails, athe except block catches the error ^^ #
+# as in Python re-assigns a returned object to a new identifier. Basically, it creates an alias #
+# ^^ yield function ends a function and returns an iterator ^^ #
