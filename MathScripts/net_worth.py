81c11a94f43be85431aaf348d5b045b67b6c8516
Jennifer Santos Custodio <jsantoscustodio@appdev.yearup.org>
2024-10-22 20:43:54 -0500

    Added lab2 , bonus1

--- MathScripts/net_worth.py
+++ MathScripts/net_worth.py
@@ -1,6 +1,17 @@
-assets=10000
-debts=3000
+assets=int(input("enter total amount of money you own or control?"))
+print(assets)
+debts=int(input("enter total amount of money you owe or due to another party"))
+print(debts)
 
 net_worth=assets-debts
 
 print("Your net worth is " + "{:,}".format(net_worth))
+
+# A pitfall would be that I needed to use casting of the correct data type to convert the string to an integer for my script to run
+# As I experimented with different input values I could not enter floats and I could not enter text as a value it had to be an integer
+
+
+
+
+
+
