fc87533fea0ca5e878d6ee38a2d895f3a3c272bf
Jennifer Santos Custodio <jsantoscustodio@appdev.yearup.org>
2024-10-22 20:53:58 -0500

    Adding lab2, bonus2

--- MathScripts/area_of_rectangle.py
+++ MathScripts/area_of_rectangle.py
@@ -3,4 +3,5 @@ length=4
 
 area = width * length
 
-print("The area of the rectangle is " + str(area))
+print(f"The area of the rectangle is {area}")
+#print("The area of the rectangle is " + str(area))
