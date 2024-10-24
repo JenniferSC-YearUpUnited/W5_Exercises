8187d675999558788d8e780b9b28a478330a1df7
Jennifer Santos Custodio <jsantoscustodio@appdev.yearup.org>
2024-10-24 14:16:07 -0500

    Adding 3.B, Lab 2.

---
+++ SampleCollections/candy_store.py
@@ -0,0 +1,7 @@
+type_candy = (' Hi-Chew', " Jelly Ranchers", " Frooties")
+fruity_flavors = (' Mango', ' Blue Raspberry', ' Strawberry')
+fruit_candy = {type_candy[1] + fruity_flavors[2], type_candy[2] + fruity_flavors[0], type_candy[0] + fruity_flavors[1]}
+print(" Today's candy options include:", fruit_candy)
+print(" Today's candy options include:", fruit_candy)
+print(" Today's candy options include:", fruit_candy)
+# The order of the items as I repeat the output remains the same. 
