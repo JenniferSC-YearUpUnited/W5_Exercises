5c49d721c4c82b5ad0119aab5e12f10c2bc06532
Jennifer Santos Custodio <jsantoscustodio@appdev.yearup.org>
2024-10-27 12:23:08 -0500

    Adding script 4.A, Lab 1.

--- IfScripts/if_basics.py
+++ IfScripts/if_basics.py
@@ -1,2 +1,46 @@
-x = 100
+# define the known
+
+x = 20
 y = 20
+divison = x/y
+multi= x*y
+
+# calculate the unknown with x to 100
+
+if divison == 5:
+    print("x divided by y is 5")
+else:
+    print("are the variables set up correctly?")
+
+# calculate the unknown with x set to 1 
+if  multi == y:
+    print("now x times y is y")
+else:
+    print('"Whoops,x equals" + the value of x ')
+
+# calculate the unknown with x set to 10
+if x < y:
+    print("x is less than y")  
+else:
+    print("uh oh, x is not less than y")
+
+# calculate the unknown with x being double the value
+if x > y:
+    print("how is x greater than??") 
+else:    
+    print("x is NOT greater than y")   
+
+print("The final value of x is 20 and the final value of y is 20")
+
+    
+   
+    
+   
+
+
+
+    
+
+
+
+
