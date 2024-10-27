029a8af9060a4bc848d4ca89cee81f3b18281938
Jennifer Santos Custodio <jsantoscustodio@appdev.yearup.org>
2024-10-27 12:31:47 -0500

    Adding comments to script.

--- hello.py
+++ hello.py
@@ -1,15 +1,22 @@
+# writing my first program
 print('Hello world!')
 
-
+# name variable containing a string
 message = 'Hello world!'
+# print message
 print(message)
 
+# creating two variables
 dollars = '3'
 cents = '.50'
+
+# testing my variables
 print(dollars + cents)
 
+# I want to know my datatype
 print(type(dollars+cents))
 
+# bonus
 word1 = '3 dollars'
 word2 = 'fifty cents'
 print (word1 +" "+ word2)
