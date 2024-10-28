67cdff8636520053f522e47b119ea797c621cf1a
Jennifer Santos Custodio <jsantoscustodio@appdev.yearup.org>
2024-10-27 23:44:37 -0500

    Adding script 4.B, Lab 2.

---
+++ IfScripts/greeting.py
@@ -0,0 +1,22 @@
+# define the known values
+current_hour = 23
+
+# calculate the unknown and display result
+# tried match/case but could not get any comparison operators to work
+if current_hour <= 9:
+    print('Good morning!')
+elif current_hour <= 16:
+    print('Good day!')
+elif current_hour <= 23:
+    print('Good evening!')    
+else: 
+    print('Invalid Military Hour Only- No Minutes!')    
+
+
+
+
+
+
+
+     
+    
