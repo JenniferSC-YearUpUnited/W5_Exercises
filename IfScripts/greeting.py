3ff7b37f8f58c64552a8a5e7846f2b4c14f6b72d
Jennifer Santos Custodio <jsantoscustodio@appdev.yearup.org>
2024-10-28 15:08:13 -0500

    Updated script with last question.

--- IfScripts/greeting.py
+++ IfScripts/greeting.py
@@ -6,16 +6,41 @@
 #if current_hour <=9:
     #print('Good morning!')
 #elif current_hour <= 16:
-   # print('Good day!')
+#   print('Good day!')
 #elif current_hour <= 10:
-  #  print('Good evening!')    
-#elif current_hour >=22:
+ # print('Good evening!')    
+#elif current_hour >=22 and (current_hour <= 4):
  #   print(' What are you doing up so late???')
 #else: 
-    #print('Invalid Military Hour Only- No Minutes!')    
+ #   print('Invalid Military Hour Only- No Minutes!')    
 
 
 ## Need to work on last question 
+
+import time
+
+current_hr = time.strftime("%H:%M""%p")
+print(current_hr)
+
+current_hr1 = int(time.strftime("%H"))
+
+morning_greeting = "Good morning!"
+day_greeting = "Good day!"
+evening_greeting = "Good evening!" 
+latenight_greeting = "What are you doing up so late??"
+
+if 5 >= current_hr1 <=9:
+    print(morning_greeting)
+elif current_hr1 <= 16:
+   print(day_greeting)
+elif current_hr1 <= 10:
+  print(evening_greeting)    
+elif 22 >= current_hr1 <= 4:
+    print(latenight_greeting)
+else:
+   print("Get some rest!")    
+
+#try it again tonight to see if it prints the right message for evening and late night
 
 
 
