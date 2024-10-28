e04854fb3c4744679305da1eda8e3ab8e8da87a1
Jennifer Santos Custodio <jsantoscustodio@appdev.yearup.org>
2024-10-28 10:43:36 -0500

    commented code out

--- IfScripts/greeting.py
+++ IfScripts/greeting.py
@@ -1,17 +1,21 @@
 # define the known values
-current_hour = 23
+#current_hour = 4
 
 # calculate the unknown and display result
 # tried match/case but could not get any comparison operators to work
-if current_hour <= 9:
-    print('Good morning!')
-elif current_hour <= 16:
-    print('Good day!')
-elif current_hour <= 23:
-    print('Good evening!')    
-else: 
-    print('Invalid Military Hour Only- No Minutes!')    
+#if current_hour <=9:
+    #print('Good morning!')
+#elif current_hour <= 16:
+   # print('Good day!')
+#elif current_hour <= 10:
+  #  print('Good evening!')    
+#elif current_hour >=22:
+ #   print(' What are you doing up so late???')
+#else: 
+    #print('Invalid Military Hour Only- No Minutes!')    
 
+
+## Need to work on last question 
 
 
 
