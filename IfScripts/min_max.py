68a475eb849de85c334b5272ed0e04d28fa9ab51
Jennifer Santos Custodio <jsantoscustodio@appdev.yearup.org>
2024-10-29 12:02:00 -0500

    Updating script to add more comparison expressions to test different values.

--- IfScripts/min_max.py
+++ IfScripts/min_max.py
@@ -1,9 +1,18 @@
-a = 18
-b = 21
-c = 22
+a = 500
+b = 100
+c = 1
 
 if a < b and  a < c:
-    print(f'{a} is the min')
+    print(f'{a} is the min') 
+if a > b and a > c:
+    print(f'{a} is the max')       
 if c > a and c > b:
     print(f'{c} is the max')
-# come back to this   
+if b > a and b > c:
+    print(f'{b} is the max')
+if b < a and b < c:
+    print(f'{b} is the min')  
+if c < a and c < b:
+    print(f'{c} is the min')     
+else:
+    print("invalid error!")
