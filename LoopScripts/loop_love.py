ea720da0c1f854d913661532125da525bcee3b91
Jennifer Santos Custodio <jsantoscustodio@appdev.yearup.org>
2024-10-29 15:12:15 -0500

    Adding script 4.c, Lab 1.

--- LoopScripts/loop_love.py
+++ LoopScripts/loop_love.py
@@ -1,1 +1,6 @@
-great = ["masterful, gifted, talented, excellent, outstanding"]
+great = ["masterful", "gifted", "talented", "excellent", "outstanding"]
+for counter in great:
+   print(great, "Loops are", great[0],"!")
+
+print("I <3 loops")
+ 
