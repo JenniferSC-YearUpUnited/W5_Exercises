c48cbeadbf06ff8558f027c6e558a92fb0fcc946
Jennifer Santos Custodio <jsantoscustodio@appdev.yearup.org>
2024-10-29 11:44:05 -0500

    Updated comparison operators, testing current hour output.

--- IfScripts/greeting.py
+++ IfScripts/greeting.py
@@ -25,19 +25,22 @@ print(current_hr)
 current_hr1 = int(time.strftime("%H")) #casting the display of time to a string as it is not letting me print without converting it first
 
 
-if 5 >= current_hr1 <=9:
+if current_hr1 >= 5 and current_hr1 <= 9:
     print("Good morning!") # current hours being midnight 00 -9am then between 5 and 9am
-elif 10 >= current_hr1 <= 16:
+elif current_hr1 >= 10 and current_hr1 <= 16:
    print("Good day!") # current hour being 10am through 4pm(1600)
-elif 17 >= current_hr1 <= 22:
-  print("Good evening!")  #current hours being 5pm(1700 )through midnight (000) then current hours being 5pm( 1700)through 11pm (2200)
-elif 23 >= current_hr1 <= 4:
+elif current_hr1 >= 17 and current_hr1 <= 22:
+  print("Good evening!")  #current hours being 5pm(1700 )through midnight (0000) then current hours being 5pm( 1700)through 11pm (2200)
+elif current_hr1 >= 23 and current_hr1 <= 4:
     print("What are you doing up so late??") # updating/ adding for current hours being 11pm(2300) thorugh 4am
 
 
   
 
-#try it again tonight to see if it prints the right message for evening and late night
+# try it again tonight to see if it prints the right message for evening and late night
+# not giving me the right output for the greeting based on time
+# need to change the comparison operators
+# run code again tonight
 
 
 
